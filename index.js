0dcf89dd64f52401516ff9bb719e410014531fce
urbangeek <gupta.amlan@gmail.com>
2017-07-19 17:43:02 +0530

    chance

--- index.js
+++ index.js
@@ -530,21 +530,7 @@ function setCustomDataForChooseDoctor(selectedDoctor, rootUrl){
                             "title":"View Portfolio"
                           }              
                         ]      
-                      },
-                        {
-                        "title":"amlan",
-                        "image_url":rootUrl + "/images/"+ selectedDoctor.image,
-                        "subtitle":"What is the best time that will work for you?",
-
-                        "buttons":[
-                          {
-                            "type":"web_url",
-                            "url":"https://xxx.xxx",
-                            "title":"View Portfolio"
-                          }              
-                        ]      
                       }
-                    ]
                   }
                 }                                                                       
             }
