a8a9efb4f6df9053f33ec0562f3a6a03f0796ba8
urbangeek <gupta.amlan@gmail.com>
2017-07-27 12:51:03 +0530

    confirm appointment

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const settings = JSON.parse(fs.readFileSync('data/settings.json', 'utf8'));
 const masterDepartmentList = JSON.parse(fs.readFileSync('data/departments.json', 'utf8'));
 const masterDoctorList = JSON.parse(fs.readFileSync('data/doctors.json', 'utf8'));
 var ObjectID = mongodb.ObjectID;
-const MEETING_COLLECTION = 'meeting_default';
-
 
 //var apiai = require("./module/apiai");
 //var app = apiai(settings.accessToken);
@@ -20,6 +18,7 @@ var welcomeModule = require('./module/welcome.js');
 var defaultFallbackModule = require('./module/default_fallback.js');
 var chooseDepartmentModule = require('./module/choose_department.js');
 var chooseDoctorModule = require('./module/choose_doctor.js');
+var confirmAppointmentModule = require('./module/confirm_appointment.js');
 var helper = require('./module/helper.js');
 
 const app = express();
@@ -69,15 +68,13 @@ app.post('/hook', function (req, res) {
             var requestBody = req.body;
             
             var sessionId = requestBody.sessionId;
-
-            
-            
             
             switch(requestBody.result.action){
                     
                 case 'input.welcome':
                     
-                    console.log('Fired: input.welcome');                    
+                    console.log('Fired: input.welcome');  
+                    
                     welcomeModule.sayHello(res,callback);
                     
                     break;
@@ -86,6 +83,7 @@ app.post('/hook', function (req, res) {
                 case 'input.unknown':
                     
                     console.log('Fired: input.unknown');
+                    
                     defaultFallbackModule.showFallback(res,callback);
                     
                     break;
@@ -114,18 +112,11 @@ app.post('/hook', function (req, res) {
                     
                 case 'choose.doctor.confirm':
                     
-                    console.log('Fired: choose.doctor.confirm');
-                                        
-                    var preselectedDepartmentContext = requestBody.result.contexts.filter(function(context){
-                        return context.name === 'getdoctorsbydepartment-followup';
-                    })[0];
-					
-                    var timeManager = requestBody.result.contexts.filter(function(context){
-                        return context.name === 'time-manager';
-                    })[0];
+                    /* confirming appointment */           
                     
-                    insertMeeting(preselectedDepartmentContext, timeManager, res, rootUrl);                   
-                                        
+                    console.log('Fired: choose.doctor.confirm');
+                    
+                    confirmAppointmentModule.getResponse(db,requestBody,rootUrl,res,callback);
                     
                     break;
                 
@@ -259,141 +250,6 @@ app.post('/getMeetingForDoctor', function (req, res) {
     
 });
 
-
-
-
-
-
-function insertMeeting(preselectedDepartmentContext, timeManager, res,rootUrl){
-    
-    var speech = '';
-    var returnContext = [];
-    var customData = [];
-    
-	  
-    var selectedDate = preselectedDepartmentContext.parameters.date;
-    var selectedTime = preselectedDepartmentContext.parameters.time;
-			
-			
-    if(timeManager)
-    {
-		selectedDate = selectedDate || timeManager.parameters.stack_date;
-        selectedTime = selectedTime || timeManager.parameters.stack_time;
-    }					
-    
-
-    var dateTime = moment(selectedDate + " " + selectedTime);			    
-
-    var newMeeting = {
-        "doctor_name": preselectedDepartmentContext.parameters['dept-doctors'],
-        "start_date_time": dateTime.toDate(),
-        "end_date_time": dateTime.add(30, 'm').toDate(),
-        "department": preselectedDepartmentContext.parameters.department
-    }
-
-    /* insert data to mongo*/
-
-
-    db.collection(MEETING_COLLECTION).insertOne(newMeeting, function(err, doc) {
-                
-        if (err) {
-            speech = 'Failed to book meeting!';            
-            
-        } else {
-            speech = 'Thanks for booking! See you!';
-             
-        }
-        
-        
-        /* facebook specific */
-        
-            customData = {
-                "facebook": [
-                    {
-                        "attachment":{
-                          "type":"template",
-                          "payload":{
-                            "template_type":"generic",
-                            "elements":[
-                               {
-                                "title":"Appointment booked with " + helper.getDoctorByCode(newMeeting.doctor_name).title,
-                                "image_url":rootUrl + "/images/"+ helper.getDoctorByCode(newMeeting.doctor_name).image,
-                                "subtitle":"Time: " + moment(newMeeting.start_date_time).format("MMMM Do YYYY, h:mm a")                                   
-                              }
-                            ]
-                          }
-                        }                                                                       
-                    },
-                    {
-                        "attachment":{
-                          "type":"template",
-                             "payload":{
-                                "template_type":"button",
-                                "text":"Thanks for the booking 👍 \nNeed further assistance? Talk to a representative",
-                                "buttons":[
-                                   {
-                                      "type":"phone_number",
-                                      "title":"Call Representative",
-                                      "payload":"+15105551234"
-                                   }
-                                ]
-                             }
-                        }
-                    }
-                ]
-            };
-        
-        
-            //reset all context
-        
-            returnContext = [
-                {
-                    "name":"has-nothing", 
-                    "lifespan":0, 
-                    "parameters":{}
-                },
-                {
-                    "name":"has-date", 
-                    "lifespan":0, 
-                    "parameters":{}
-                },
-                {
-                    "name":"has-time", 
-                    "lifespan":0, 
-                    "parameters":{}
-                },
-                {
-                    "name":"has-date-time", 
-                    "lifespan":0, 
-                    "parameters":{}
-                },
-                {
-                    "name":"getdoctorsbydepartment-followup", 
-                    "lifespan":0, 
-                    "parameters":{}
-                },
-                {
-                    "name":"choosedoctor-followup-2", 
-                    "lifespan":0, 
-                    "parameters":{}
-                },
-                {
-                    "name":"choosedoctor-followup", 
-                    "lifespan":0, 
-                    "parameters":{}
-                },
-                {
-                    "name":"time-manager", 
-                    "lifespan":0, 
-                    "parameters":{}
-                }
-                            
-            ];        
-        
-        callback(res,speech,returnContext,customData);
-    });
-    
-}
 
 
 function callback(res,speech,returnContext,customData){
